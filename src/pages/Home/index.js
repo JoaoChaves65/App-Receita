ecf0452d7fdc43651d01f091e7ebd277a3b06aa7
unknown <joaomarcosgol2000@gmail.com>
2023-04-06 16:11:31 -0300

    first commit

---
+++ src/pages/Home/index.js
@@ -0,0 +1,63 @@
+
+import { View, Text, StyleSheet, SafeAreaView, TextInput } from "react-native"
+
+import { Ionicons } from '@expo/vector-icons'
+
+import { Logo } from "../../components/logo"
+import { TouchableHighlightBase } from "react-native"
+import { TouchableOpacity } from "react-native"
+
+export function Home () {
+    return(
+        <SafeAreaView style={styles.container}>
+            <Logo/>
+
+            <Text style={styles.title}>Encontre a receita</Text>
+            <Text style={styles.title}>que combina com você</Text>
+
+            <View style={styles.form}>
+                <TextInput placeholder="Digite o nome da comida..."
+                style={styles.input}
+                />
+                <TouchableOpacity>
+                    <Ionicons name="search" size={28} color="#4CBE6C"/>
+                </TouchableOpacity>
+            </View>
+        </SafeAreaView>
+    )
+}
+
+const styles = StyleSheet.create({
+    container:{
+        flex: 1,
+        backgroundColor: "#F3F9FF",
+        paddingTop: 46,
+        paddingStart: 14,
+        paddingEnd: 14,
+    },
+    title:{
+        fontSize: 28,
+        fontWeight: "bold",
+        color: "#0e0e0e"
+    },
+    form:{
+        backgroundColor: "#FFF",
+        width: "100%",
+        borderRadius: 8,
+        marginTop: 16,
+        marginBottom: 16,
+        borderWidth: 1,
+        borderColor: "#ECECEC",
+        paddingLeft: 8,
+        paddingRight: 8,
+        flexDirection: "row",
+        alignItems: "center",
+        justifyContent: "space-between"
+    },
+    input:{
+        width: "90%",
+        maxWidth: "90%",
+        height: 54
+        
+    }
+})
